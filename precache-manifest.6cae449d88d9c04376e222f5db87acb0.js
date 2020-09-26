22083d2c3cd0ef27b3d85b8628efb637f123be4c
nautia <nautia@gmail.com>
2020-09-26 23:57:04 +0900

    Updates

---
+++ precache-manifest.6cae449d88d9c04376e222f5db87acb0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "787642bd4e1ebc7771b6d7a1b34f5412",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f5027a99277c57bdf650",
+    "url": "/portfolio/static/css/main.01732121.chunk.css"
+  },
+  {
+    "revision": "fa5967c1ed04dc17fd55",
+    "url": "/portfolio/static/js/2.e7f0ceea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.e7f0ceea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5027a99277c57bdf650",
+    "url": "/portfolio/static/js/main.e6afd13d.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "be263e64dd8f92446119ab93a089e87a",
+    "url": "/portfolio/static/media/binary.be263e64.jpg"
+  },
+  {
+    "revision": "94a4f27061227ec75e6e0ecf579a1c78",
+    "url": "/portfolio/static/media/react.94a4f270.png"
+  },
+  {
+    "revision": "670e094ad84e0841e66d2345867e5f29",
+    "url": "/portfolio/static/media/tmp.670e094a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b93887242e76d3845625896b233e4c7a.js"
+  "/portfolio/precache-manifest.6cae449d88d9c04376e222f5db87acb0.js"
 );
 
 self.addEventListener('message', (event) => {
